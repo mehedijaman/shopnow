1cee5f3b94ef11e95ea9585fe6061c869f7c3dff
Mehedi Jaman <mail4mjaman@gmail.com>
2024-11-08 06:02:24 +0600

    Packages updated

--- resources/js/Components/Auth/AppAuthLogo.vue
+++ resources/js/Components/Auth/AppAuthLogo.vue
@@ -1,5 +1,5 @@
 <template>
-    <a href="#" class="mb-6 flex items-center">
+    <a :href="route('adminAuth.loginForm')" class="mb-6 flex items-center">
         <span
             class="rounded-md bg-blue-500 px-4 py-1 text-3xl font-extrabold text-skin-neutral-3 hover:text-skin-neutral-6 dark:text-skin-neutral-1 dark:hover:text-skin-primary-9"
         >
