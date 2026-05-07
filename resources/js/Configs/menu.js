9cd7178fe4c0d3885984c1fbba232e23698934c9
Mehedi Jaman <mail4mjaman@gmail.com>
2026-05-07 15:24:44 +0600

    update: dependencies

--- resources/js/Configs/menu.js
+++ resources/js/Configs/menu.js
@@ -7,12 +7,12 @@ export default {
             icon: 'ri-dashboard-line',
             link: route('dashboard.index')
         },
-        // {
-        //     label: 'Contact Messages',
-        //     permission: 'contact-message-list',
-        //     icon: 'ri-mail-line',
-        //     link: route('contactMessage.index')
-        // },
+        {
+            label: 'Contact Messages',
+            permission: 'contact-message-list',
+            icon: 'ri-mail-line',
+            link: route('contactMessage.index')
+        },
         {
             label: 'Order Management',
             permission: 'order-menu',
@@ -55,75 +55,75 @@ export default {
                     link: route('productBrand.index')
                 }
             ]
+        },
+
+        {
+            label: 'Customer Management',
+            permission: 'customer-menu',
+            children: [
+                {
+                    label: 'Customers',
+                    permission: 'customer-list',
+                    icon: 'ri-draft-line',
+                    link: route('customer.index')
+                }
+            ]
+        },
+
+        {
+            label: 'Blog',
+            permission: 'Blog',
+            children: [
+                {
+                    label: 'Posts',
+                    permission: 'Blog: Post - List',
+                    icon: 'ri-draft-line',
+                    link: route('blogPost.index')
+                },
+                {
+                    label: 'Categories',
+                    permission: 'Blog: Category - List',
+                    icon: 'ri-folders-line',
+                    link: route('blogCategory.index')
+                },
+                {
+                    label: 'Tags',
+                    permission: 'Blog: Tag - List',
+                    icon: 'ri-price-tag-3-line',
+                    link: route('blogTag.index')
+                },
+                {
+                    label: 'Authors',
+                    permission: 'Blog: Author - List',
+                    icon: 'ri-team-line',
+                    link: route('blogAuthor.index')
+                }
+            ]
+        },
+
+        {
+            label: 'Access Control List',
+            permission: 'Acl',
+            children: [
+                {
+                    label: 'Users',
+                    permission: 'Acl: User - List',
+                    icon: 'ri-user-line',
+                    link: route('user.index')
+                },
+                {
+                    label: 'Permissions',
+                    permission: 'Acl: Permission - List',
+                    icon: 'ri-shield-keyhole-line',
+                    link: route('aclPermission.index')
+                },
+                {
+                    label: 'Roles',
+                    permission: 'Acl: Role - List',
+                    icon: 'ri-account-box-line',
+                    link: route('aclRole.index')
+                }
+            ]
         }
-
-        // {
-        //     label: 'Customer Management',
-        //     permission: 'customer-menu',
-        //     children: [
-        //         {
-        //             label: 'Customers',
-        //             permission: 'customer-list',
-        //             icon: 'ri-draft-line',
-        //             link: route('customer.index')
-        //         }
-        //     ]
-        // },
-
-        // {
-        //     label: 'Blog',
-        //     permission: 'Blog',
-        //     children: [
-        //         {
-        //             label: 'Posts',
-        //             permission: 'Blog: Post - List',
-        //             icon: 'ri-draft-line',
-        //             link: route('blogPost.index')
-        //         },
-        //         {
-        //             label: 'Categories',
-        //             permission: 'Blog: Category - List',
-        //             icon: 'ri-folders-line',
-        //             link: route('blogCategory.index')
-        //         },
-        //         {
-        //             label: 'Tags',
-        //             permission: 'Blog: Tag - List',
-        //             icon: 'ri-price-tag-3-line',
-        //             link: route('blogTag.index')
-        //         },
-        //         {
-        //             label: 'Authors',
-        //             permission: 'Blog: Author - List',
-        //             icon: 'ri-team-line',
-        //             link: route('blogAuthor.index')
-        //         }
-        //     ]
-        // },
-
-        // {
-        //     label: 'Access Control List',
-        //     permission: 'Acl',
-        //     children: [
-        //         {
-        //             label: 'Users',
-        //             permission: 'Acl: User - List',
-        //             icon: 'ri-user-line',
-        //             link: route('user.index')
-        //         },
-        //         {
-        //             label: 'Permissions',
-        //             permission: 'Acl: Permission - List',
-        //             icon: 'ri-shield-keyhole-line',
-        //             link: route('aclPermission.index')
-        //         },
-        //         {
-        //             label: 'Roles',
-        //             permission: 'Acl: Role - List',
-        //             icon: 'ri-account-box-line',
-        //             link: route('aclRole.index')
-        //         }
-        //     ]
-        // }
     ]
 }
