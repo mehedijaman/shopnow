7abc8e6c56e003d386192c1ed73eab4ab2e91b41
Mehedi Jaman <mail4mjaman@gmail.com>
2026-05-09 15:34:52 +0600

    update: homepage product quantity

--- modules/Index/Http/Controllers/IndexController.php
+++ modules/Index/Http/Controllers/IndexController.php
@@ -20,7 +20,7 @@ class IndexController extends SiteController
                 $query->where('active', true)
                     ->with('category')
                     ->latest()
-                    ->limit(8);
+                    ->limit(40);
             }])
             ->get();
 
