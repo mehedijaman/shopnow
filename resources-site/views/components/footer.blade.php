e3768afe888ff87056544ad199c96654af016d35
Mehedi Jaman <mail4mjaman@gmail.com>
2026-09-23 14:40:16 +0600

    fix: enlarge footer logo on mobile

--- resources-site/views/components/footer.blade.php
+++ resources-site/views/components/footer.blade.php
@@ -30,9 +30,9 @@
             <div class="space-y-5">
                 <a href="{{ route('site.index') }}" class="block text-center sm:inline-block">
                     @if ($footerLogo)
-                        <img src="{{ $footerLogo }}" alt="{{ $footerSiteName }}" class="h-16 w-auto max-w-[240px] sm:h-20 sm:max-w-[280px] object-contain" onerror="this.src='/logo.png'" />
+                        <img src="{{ $footerLogo }}" alt="{{ $footerSiteName }}" class="h-24 w-auto max-w-[300px] sm:h-20 sm:max-w-[280px] object-contain" onerror="this.src='/logo.png'" />
                     @else
-                        <img src="{{ asset('logo.png') }}" alt="{{ $footerSiteName }}" class="h-16 w-auto max-w-[240px] sm:h-20 sm:max-w-[280px] object-contain" />
+                        <img src="{{ asset('logo.png') }}" alt="{{ $footerSiteName }}" class="h-24 w-auto max-w-[300px] sm:h-20 sm:max-w-[280px] object-contain" />
                     @endif
                 </a>
 
